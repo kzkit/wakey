ef92d37114e7a1b6e130f091a31f0fe11bae654c
Zhen Kit Kong <zhenkitkong@gmail.com>
2026-01-25 20:49:23 +0800

    Initial Commit

---
+++ Matcha/MatchaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchaApp.swift
+//  Matcha
+//
+//  Created by Zhen Kit Kong on 25/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct MatchaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
